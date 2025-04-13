586d27c09319d49941a5a142b3bd2ffeb410b1ab
Priyanshu <Priyanshubaral9562@gmail.com>
2025-04-13 22:50:53 +0530

    FarmeasyPortal

---
+++ src/test/java/com/project/farmeasyportal/FarmeasyPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.farmeasyportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmeasyPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
