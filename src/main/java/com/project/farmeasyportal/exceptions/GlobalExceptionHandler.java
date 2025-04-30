e976d1e4b21802b02dd6cae35b03d95922093a2a
Priyanshu <Priyanshubaral9562@gmail.com>
2025-04-30 21:59:57 +0530

    FarmeasyPortal

--- src/main/java/com/project/farmeasyportal/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/project/farmeasyportal/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,9 @@
 package com.project.farmeasyportal.exceptions;
 
+import com.project.farmeasyportal.payloads.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -7,8 +11,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public RuntimeException resourceNotFoundExceptionHandler(ResourceNotFoundException ex) {
-        return new RuntimeException(ex.getMessage());
+    public ResponseEntity<ApiResponse> resourceNotFoundExceptionHandler(ResourceNotFoundException ex) {
+        ApiResponse apiResponse = ApiResponse.builder()
+                .message(ex.getMessage())
+                .build();
+        return new ResponseEntity<>(apiResponse, HttpStatus.NOT_FOUND);
     }
 
 }
