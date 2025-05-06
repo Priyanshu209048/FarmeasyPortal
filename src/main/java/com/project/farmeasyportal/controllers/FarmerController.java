62af5268b294cf00a32e34deaba1850a1681d6d5
Priyanshu <Priyanshubaral9562@gmail.com>
2025-05-06 23:11:43 +0530

    FarmeasyPortal

--- src/main/java/com/project/farmeasyportal/controllers/FarmerController.java
+++ src/main/java/com/project/farmeasyportal/controllers/FarmerController.java
@@ -1,14 +1,18 @@
 package com.project.farmeasyportal.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.project.farmeasyportal.entities.Farmer;
+import com.project.farmeasyportal.dao.LoanFormDao;
+import com.project.farmeasyportal.entities.LoanForm;
 import com.project.farmeasyportal.payloads.ApiResponse;
 import com.project.farmeasyportal.payloads.FarmerDTO;
 import com.project.farmeasyportal.payloads.LoanFormDTO;
 import com.project.farmeasyportal.services.FarmerService;
 import jakarta.validation.Valid;
+import lombok.NonNull;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +20,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
@@ -25,11 +30,13 @@ import java.util.List;
 public class FarmerController {
 
     private final FarmerService farmerService;
+    private final LoanFormDao loanFormDao;
     /*private static final Logger log = LoggerFactory.getLogger(FarmerController.class);*/
 
     @Autowired
-    public FarmerController(FarmerService farmerService) {
+    public FarmerController(FarmerService farmerService, LoanFormDao loanFormDao) {
         this.farmerService = farmerService;
+        this.loanFormDao = loanFormDao;
     }
 
     private ResponseEntity<?> checkFarmerExists(String farmerID) {
@@ -98,6 +105,40 @@ public class FarmerController {
         this.farmerService.submitForm(loanFormDTO, file, originalFileName, farmerDTO.getId());
 
         return new ResponseEntity<>("Loan form submitted successfully.", HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/updateForm", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<?> updateLoanFormProcess(@RequestPart("loanFormDTO") @Valid @NonNull String loanFormJson,
+                                                   @RequestPart("documents") MultipartFile file,
+                                                   Authentication authentication) throws IOException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        LoanFormDTO loanFormDTO = objectMapper.readValue(loanFormJson, LoanFormDTO.class);
+
+        if (!loanFormDTO.getEmail().equals(authentication.getName())) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Unauthorized access.");
+        }
+
+        FarmerDTO farmerDTO = this.farmerService.getFarmerByEmail(authentication.getName());
+        String originalFileName = file.isEmpty() ? loanFormDTO.getPdfName() : file.getOriginalFilename();
+
+        LoanFormDTO updatedForm = this.farmerService.updateLoanForm(loanFormDTO, file, originalFileName, farmerDTO.getId());
+
+        return new ResponseEntity<>(updatedForm, HttpStatus.OK);
+    }
+
+    @GetMapping("/getFormDetail")
+    public ResponseEntity<?> getLoanFormDetail(Authentication authentication) {
+        LoanFormDTO loanFormDTO = this.farmerService.getLoanFormByEmail(authentication.getName());
+        return new ResponseEntity<>(loanFormDTO, HttpStatus.OK);
+    }
+
+    @GetMapping("/download")
+    @ResponseBody
+    public FileSystemResource download(@Param(value = "id") Integer id) {
+        LoanForm loanForm = loanFormDao.findById(id).orElse(null);
+        assert loanForm != null;
+        return new FileSystemResource(new File(System.getProperty("user.dir") + "/src/main/resources/static/documents" + File.separator + loanForm.getPdfName()));
     }
 
 }
